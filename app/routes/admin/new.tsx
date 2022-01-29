3181123c24b5f16fa24b8feaca7eedb1c49e271d
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-01-29 22:54:26 +0900

    送信中の場合ボタンの文字を変えるようにした

--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -1,4 +1,4 @@
-import { useActionData, redirect, Form } from "remix";
+import { useActionData, redirect, Form, useTransition } from "remix";
 import type { ActionFunction } from "remix";
 import { createPost } from "~/post";
 import invariant from "tiny-invariant";
@@ -10,6 +10,8 @@ type PostError = {
 };
 
 export const action: ActionFunction = async ({ request }) => {
+    await new Promise(res => setTimeout(res, 1000));
+
     const formData = await request.formData()
 
     const title = formData.get("title")
@@ -36,6 +38,7 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function NewPost() {
     const errors = useActionData()
+    const transition = useTransition()
 
     return (
         <Form method="post">
@@ -66,7 +69,11 @@ export default function NewPost() {
                 <textarea id="markdown" rows={20} name="markdown" />
             </p>
             <p>
-                <button type="submit">Create Post</button>
+                <button type="submit">
+                    {transition.submission
+                        ? "Creating..."
+                        : "Create Post"}
+                </button>
             </p>
         </Form>
     );
