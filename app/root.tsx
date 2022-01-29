8832e1568512062b58214824d12ab7240ac4d345
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-01-29 14:17:01 +0900

    postsページを作成した

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration
 } from "remix";
 import type { MetaFunction } from "remix";
+import { Link } from "remix";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
@@ -26,6 +27,7 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
+        <Link to="/posts">Posts</Link>
       </body>
     </html>
   );
